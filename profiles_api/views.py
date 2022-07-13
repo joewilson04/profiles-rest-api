61bdd4274cbe32140882789beff9387c4bd56d8c
Joe <joebot24@outlook.com>
2022-07-13 12:16:24 +0100

    Created a post method that can accept and validate an input

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloAPIView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(relf, request, format=None):
         """Returns a list of APIView features"""
@@ -14,3 +18,17 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message':'Hello!', 'an_apiview':an_apiview})
+
+    def post(self, request):
+        """Create a hello message with inputted name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello'
+            return Response(message + ", " + name)
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
